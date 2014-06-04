a044c7e340753cd4ee0ec923c61831db4c437ad3
sol0mka <legomushroom@gmail.com>
2014-06-04 15:01:20 +0300

    amend

--- header.php
+++ header.php
@@ -11,8 +11,8 @@ do_action( 'statsocial_pre_part_' . basename( __FILE__, '.php' ) . '_' . basenam
     <meta http-equiv="content-type" content="<?php bloginfo( 'html_type' ); ?>; charset=<?php bloginfo( 'charset' ); ?>" />
     <title><?php wp_title( '|', true, 'right' ); ?></title>
     <?php wp_head(); ?>
-    <!-- <link rel="stylesheet" href="http://dev.statsocial.com/css/main.css"> -->
-    <link rel="stylesheet" href="/website/front-end/css/main.css">
+    <link rel="stylesheet" href="http://dev.statsocial.com/css/main.css">
+    <!-- <link rel="stylesheet" href="/website/front-end/css/main.css"> -->
     <link href="http://fonts.googleapis.com/css?family=Open+Sans:400italic,400,300,600,700,800" rel="stylesheet" type="text/css">
   </head>
   <body <?php body_class(); ?>>
